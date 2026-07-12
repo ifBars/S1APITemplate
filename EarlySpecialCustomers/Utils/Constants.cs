e7f57f6554d0a4ddc6cb3966a2542af513fe47e6
ifBars <114284668+ifBars@users.noreply.github.com>
2026-07-11 21:41:43 -0600

    Add mod constants

---
+++ EarlySpecialCustomers/Utils/Constants.cs
@@ -0,0 +1,22 @@
+namespace EarlySpecialCustomers.Utils;
+
+public static class Constants
+{
+    public const string ModName = "Early Special Customers";
+    public const string ModVersion = "0.1.0";
+    public const string ModAuthor = "Bars";
+    public const string PreferencesCategory = "EarlySpecialCustomers";
+
+    public static class Defaults
+    {
+        public const bool IntroMessagesEnabled = true;
+        public const bool MilestoneMessagesEnabled = true;
+        public const bool DebugLogsEnabled = false;
+    }
+
+    public static class Game
+    {
+        public const string Studio = "TVGS";
+        public const string Name = "Schedule I";
+    }
+}
